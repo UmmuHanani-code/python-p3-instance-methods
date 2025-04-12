a5e5333f75dd3dce78e6eb946721acd415c02ce2
Nuria Mohamed Hussein <nuria.hussein@student.moringaschool.com>
2025-04-12 21:09:10 +0300

    commit instance method assignment

--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
 
 class Person:
-    # Class body goes here
+      def talk(self):
+            print("Hello World!")
 
-    #Instance method definition
-    pass
+      def walk(self):
+            print("The person is walking.")
+
+
+lucy = Person()
+lucy.talk()
+lucy.walk()
+
+anna = Person()
+anna.talk()
+anna.walk()
+
